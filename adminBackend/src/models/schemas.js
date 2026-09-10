75d6ae677241e7a9a5b6c1801de1c348bccad4a3
codebyprajjwal02 <sprajjwalsingh230@gmail.com>
2026-09-10 09:55:40 +0000

    made userBackend

--- adminBackend/src/models/schemas.js
+++ adminBackend/src/models/schemas.js
@@ -13,6 +13,98 @@ const schemaSchema = new mongoose.Schema(
         category:{
             type:String,
             required:true
+        },
+        ministry: {
+            type: String,
+            required: true
+        },
+        description: {
+            type: String,
+            required: true
+        },
+        benefits: [
+            {
+                type: String
+            }
+        ],
+        eligibiltyCriteria: {
+            minAge: {
+                type: Number,
+                default: 0
+            },
+            maxAge: {
+                type: Number,
+                default: 100
+            },
+            genderAllowed: [
+                {
+                    type: String
+                }
+            ],
+            maxIncomeLimit: {
+                type: Number
+            },
+            categoryAllowed: [
+                {
+                    type: String
+                }
+            ],
+            occupations: [
+                {
+                    type: String
+                }
+            ],
+            residenceState: [
+                {
+                    type: String
+                }
+            ]
+        },
+        benefit: {
+            type: {
+                type: String
+            },
+            amount: {
+                type: Number
+            },
+            description: {
+                type: String
+            }
+        },
+        documentsRequired: [
+            {
+                type: String
+            }
+        ],
+        application: {
+            mode: {
+                type: String,
+                enum: ['Online','Offline','Both']
+            },
+            officialLink: {
+                type: String
+            }
+        },
+        blockers:[
+            {
+                type: {
+                    type: String
+                },
+                description: {
+                    type: String
+                },
+                solution: {
+                    type: String
+                }
+            }
+        ],
+        isPublished: {
+            type: Boolean,
+            default: true
         }
+    },
+    {
+        timestamps: true
     }
-)
+);
+export defaultmongoose.model('Scheme',schemeScheme);
