27962fa56752038c58b288e50e9fc11cda28c069
iharshkumar <srivastavaharsh1108@gmail.com>
2026-09-10 13:11:17 +0000

    Backend done:Harsh

---
+++ admin-backend/src/models/AdminUser.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const adminUserSchema=new mongoose.Schema(
+    {
+        name:{type:String,required:true},
+        email:{type:String,required:true,unique:true,lowercase:true},
+        passwordHash:{tyep:String,required:true},
+        role:{type:String,enum:['superadmin','editor'],default:'superadmin'}
+    },{
+        timestamps:true
+    }
+);
+
+export default mongoose.model('AdminUser',adminUserSchema);
