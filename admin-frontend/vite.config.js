a96b141bc0c0c665693be6134f564439b8b094da
iharshkumar <srivastavaharsh1108@mail.com>
2026-09-11 03:49:51 +0000

    vite

--- admin-frontend/vite.config.js
+++ admin-frontend/vite.config.js
@@ -1,11 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from 'tailwindcss/vite'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-    plugins:[react(),tailwindcss()],
-    server:{
-        host:'0.0.0.0',
-        allowedHosts: true
-    }
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
+  server: {
+    host: '0.0.0.0',
+    allowedHosts: ['5174-kode-ws-c68361e34.hebbale.academy'],
+  },
 })
