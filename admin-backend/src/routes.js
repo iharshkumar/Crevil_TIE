31d7f0c1c575e411e6dce28913a5d51bbd24a0ff
iharshkumar <srivastavaharsh1108@mail.com>
2026-09-11 04:07:03 +0000

    final

--- admin-backend/src/routes.js
+++ admin-backend/src/routes.js
@@ -3,7 +3,7 @@ import express from 'express';
 import cors from 'cors';
 import { connectDB } from './config/db.js';
 import schemeAdminRoutes from './routes/schemeAdminRoute.js';
-import schemeAdminRoutes from './routes/schemeAdminRoute.js';
+// import schemeAdminRoutes from './routes/schemeAdminRoute.js';
 
 const app = express();
 
